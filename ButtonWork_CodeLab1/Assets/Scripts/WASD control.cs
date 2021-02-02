f9aff5fb6ca2e821fb4143b45188f798e4c07c33
Walid <ar6749@nyu.edu>
2021-02-01 21:00:13 -0500

    Unity File

---
+++ ButtonWork_CodeLab1/Assets/Scripts/WASD control.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDcontrol : MonoBehaviour
+{
+
+    public float forceAmt;
+
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * forceAmt);
+        }
+    }
+}
